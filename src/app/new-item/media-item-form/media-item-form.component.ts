2b13501a4d695fefdfcb16db76f5b494e1cf1f0c
Kundan Prajapati <prajapatikundan0@gmail.com>
2022-08-05 11:27:59 +0530

    feat: Create basic form group without validation

--- src/app/new-item/media-item-form/media-item-form.component.ts
+++ src/app/new-item/media-item-form/media-item-form.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
   templateUrl: './media-item-form.component.html',
-  styleUrls: ['./media-item-form.component.css']
+  styleUrls: ['./media-item-form.component.css'],
 })
 export class MediaItemFormComponent implements OnInit {
+  mediaItemForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.mediaItemForm = this.fb.group({
+      medium: ['Movies'],
+      name: [''],
+      category: ['Action'],
+      year: [''],
+    });
   }
 
+  onSubmit(mediaItem: any) {
+    console.log(mediaItem);
+  }
 }
--- src/app/new-item/new-item.module.ts
+++ src/app/new-item/new-item.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { NewItemRoutingModule } from './new-item-routing.module';
 import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    MediaItemFormComponent
-  ],
-  imports: [
-    CommonModule,
-    NewItemRoutingModule
-  ]
+  declarations: [MediaItemFormComponent],
+  imports: [CommonModule, NewItemRoutingModule, ReactiveFormsModule],
 })
-export class NewItemModule { }
+export class NewItemModule {}
